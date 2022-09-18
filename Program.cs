4ba3dd4897a3f29a3034e4804c25bef9bf99e5f0
«Anton» <asavchenko.1988@mail.ru>
2022-09-18 18:39:55 +0300

    Описан ввод размерности массива и заполнение массива пользователем

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Input(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int dimension = Input("Введите размерность массива");
+string [] array = new string[dimension];
+void FillArray()
+{
+    for (int i = 0; i < dimension; i++)
+    {
+        Console.WriteLine("Введите {0}-й элемент массива", i + 1);
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+FillArray();
