77b2027925d111183b76d7c0191a5cd68674524f
«Anton» <asavchenko.1988@mail.ru>
2022-09-20 00:30:48 +0300

    Написан код решения задачи

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
     return number;
 }
 int dimension = Input("Введите размерность массива");
-string [] array = new string[dimension];
+string[] array = new string[dimension];
+string[] SortArray = new string[array.Length];
 void FillArray()
 {
     for (int i = 0; i < dimension; i++)
@@ -14,5 +15,23 @@ void FillArray()
         array[i] = Console.ReadLine();
     }
     Console.WriteLine("[{0}]", string.Join(", ", array));
+    Console.WriteLine();
 }
 FillArray();
+void PrintArrayWithIF(string[] array, string[] SortArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            SortArray[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref SortArray, count);
+    Console.WriteLine("[{0}]", string.Join(", ", SortArray));
+   
+}
+PrintArrayWithIF(array, SortArray);
+
